37f6457d15c86277d45704ac94b24c7766d2baf2
Ubuntu <ubuntu@ip-10-38-37-162.ec2.internal>
2011-07-13 11:09:00 +0000

    add run script

---
+++ run-topic-corpus.sh
@@ -0,0 +1,6 @@
+pig -x local -b examples/topic-corpus/01_count_child_topics.pig
+pig -x local -b examples/topic-corpus/02_find_grounded_topics.pig
+pig -x local -b examples/topic-corpus/03_find_descendants.pig
+pig -x local -b examples/topic-corpus/04_find_grounded_topics_articles.pig
+pig -x local -b examples/topic-corpus/05_build_grounded_ancestry.pig
+pig -x local -b examples/topic-corpus/06_extract_aggregate_topic_abstracts.pig
